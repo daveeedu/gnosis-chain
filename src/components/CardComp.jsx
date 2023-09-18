e5a34d51ed9c83de47c7e65bae889adb06fbd2fb
David Edu <daveeedu@gmail.com>
2023-09-18 11:55:07 +0100

    update landing page

--- src/components/CardComp.jsx
+++ src/components/CardComp.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { IGithub, IInstagram, ILinkedIn, ITwitter } from '../utils/icons.utils';
 
 const CardComp = ({data}) => {
   const [revealedCard, setRevealedCard] = useState(3);
@@ -8,26 +9,38 @@ const CardComp = ({data}) => {
 
 
   return (
-    <div className="flex -space-x-[8%] mt-10 justify-center  relative text-start z-20">
+    <div className="md:flex md:-space-x-[8%] mt-10 justify-center relative text-start">
     {data.map((card) => (
       <div
         key={card.id}
-        className={`bg-[#11453B] h-60 drop-shadow-2xl shadow-2xl text-white rounded-[10px] p-10 w-[30%] transform transition-transform ${
-          card.id === revealedCard ? 'translate-x-0' : 'translate-x-20'
+        className={`bg-[#11453B] h-60 drop-shadow-2xl relative  text-white rounded-[10px] p-5 lg:w-[30%] w-[95%] m-auto md:m-0 mb-3 md:mb-0 transform transition-transform ${
+          card.id === revealedCard ? 'md:translate-x-0' : 'lg:translate-x-20 md:translate-x-50'
         } cursor-pointer ${card.id === revealedCard ? 'z-[2]' : ''}`}
         onClick={() => handleCardClick(card.id)}
       >
         <h1 className=" text-[1.7em] font-bold mb-2 text-[#B4A572]">{card.title}</h1>
-        <div className="overflow-hidden text-[0.7em] text-[400] leading-[24px] text-[#B4A572]">
-          {card.id === revealedCard ? (
+       <div className='flex '>
+       <div className="overflow-hidden  text-[0.7em] text-[400] leading-[24px] text-[#B4A572]">
             <p>{card.content}</p>
-          ) : (
-            <p>{card.content.slice(0, card.content.length / 2)}</p>
-          )}
+            <div className='flex jsutify-betwen gap-2 absolute bottom-5'>
+              <a href={card?.links?.twitter} >
+              <img src={ITwitter} alt='twitter' className='w-[1.5em] h-[1.5em] '/>
+              </a>
+              <a href={card?.links?.linkedin} >
+              <img src={ILinkedIn} alt='linkedin' className='w-[1.5em] h-[1.5em]'/>
+              </a>
+              <a href={card?.links?.instagram} >
+              <img src={IInstagram} alt='instagram' className='w-[1.5em] h-[1.5em]'/>
+              </a>
+              <a href={card?.links?.github} >
+              <img src={IGithub} alt='github' className='w-[1.5em] h-[1.5em]'/>
+              </a>
+              </div>
         </div>
-        <div className='flex justify-end mb-3 fixed'>
-        <img src={card?.img} alt='avater' className='w-[35%] rounded-2xl' />
+        <div className='flex justify-end mb-3 '>
+        <img src={card?.img} alt='avater' className='w-[75%] rounded-2xl' />
             </div>
+       </div>
       </div>
     ))}
   </div>
--- src/utils/icons.utils.js
+++ src/utils/icons.utils.js
@@ -4,6 +4,11 @@ import GreenTick from "../assets/greenTick.svg"
 import TalentOne from "../assets/fifth-top-talent.svg"
 import TalentTwo from "../assets/eleventh-top-talent.svg"
 import TalentThree from "../assets/eighth-top-talent.svg"
+import Twitter from "../assets/twitter.png"
+import LinkedIn from "../assets/LinkedIn.png"
+import Instagram from "../assets/instagram.svg"
+import Github from "../assets/Github.png"
+
 
 
 
@@ -14,6 +19,10 @@ export const IGreenTick = GreenTick
 export const ITalentOne = TalentOne
 export const ITalentTwo = TalentTwo
 export const ITalentThree = TalentThree
+export const ITwitter = Twitter
+export const ILinkedIn = LinkedIn
+export const IInstagram = Instagram
+export const IGithub = Github
 
 
 
